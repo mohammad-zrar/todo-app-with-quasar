3cccb4c63450ac6864196ae78748cbd4229719eb
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-03-05 16:13:15 +0300

    Feat(Todo.vue): Adding offline todolist is completed

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,12 +1,27 @@
 <template>
-  <q-page class="q-pa-md">
+  <q-page class="bg-grey-3 column">
+    <div class="row q-pa-sm bg-primary">
+      <q-input
+        v-model="newTask"
+        class="col"
+        bg-color="white"
+        filled
+        placeholder="Add task"
+        dense
+      >
+        <template v-slot:append>
+          <q-btn @click="addTask()" round dense flat icon="add" />
+        </template>
+      </q-input>
+    </div>
     <q-list separator bordered class="bg-white">
       <q-item
-        v-for="task in tasks"
+        v-for="(task, index) in tasks"
         @click="task.done = !task.done"
         v-ripple
         :key="task.title"
         clickable
+        :class="{ 'done bg-blue-1': task.done }"
       >
         <q-item-section avatar>
           <q-checkbox v-model="task.done" color="primary" />
@@ -14,21 +29,61 @@
         <q-item-section>
           <q-item-label>{{ task.title }}</q-item-label>
         </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn
+            flat
+            round
+            color="primary"
+            @click.stop="deleteTask(index)"
+            icon="delete"
+          />
+        </q-item-section>
       </q-item>
-    </q-list></q-page
-  >
+    </q-list>
+    <div v-if="!tasks.length" class="no-tasks absolute-center">
+      <q-icon name="check" size="100px" color="primary"></q-icon>
+      <div class="text-h5 text-primary text-center">No Tasks</div>
+    </div>
+  </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-
+import { useQuasar } from "quasar";
+const $q = useQuasar();
 interface Task {
   title: string;
   done: boolean;
 }
 
-const tasks = ref<Task[]>([
-  { title: "First of all just do it", done: false },
-  { title: "Second of all do it again", done: false },
-]);
+const newTask = ref<string>("");
+
+const tasks = ref<Task[]>([]);
+function addTask() {
+  tasks.value.push({ title: newTask.value, done: false });
+  newTask.value = "";
+}
+function deleteTask(index: number) {
+  $q.dialog({
+    title: "Confirm",
+    message: "Really delete?",
+    cancel: true,
+    persistent: true,
+  }).onOk(() => {
+    tasks.value.splice(index, 1);
+    $q.notify("Task deleted");
+  });
+}
 </script>
+
+<style class="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+.no-tasks {
+  opacity: 0.5;
+}
+</style>
