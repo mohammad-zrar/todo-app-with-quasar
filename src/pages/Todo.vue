71428e51ef9515e6c8a2f1705bd9d089fa749464
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-03-05 12:59:00 +0300

    feat(Todo list): Adding to do list to show tasks list

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,7 +1,34 @@
 <template>
-  <q-page>
-    <h5>Todo</h5>
-  </q-page>
+  <q-page class="q-pa-md">
+    <q-list separator bordered class="bg-white">
+      <q-item
+        v-for="task in tasks"
+        @click="task.done = !task.done"
+        v-ripple
+        :key="task.title"
+        clickable
+      >
+        <q-item-section avatar>
+          <q-checkbox v-model="task.done" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list></q-page
+  >
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface Task {
+  title: string;
+  done: boolean;
+}
+
+const tasks = ref<Task[]>([
+  { title: "First of all just do it", done: false },
+  { title: "Second of all do it again", done: false },
+]);
+</script>
