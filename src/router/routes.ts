127a0fdaa888253561e3e0a128cac669c0418ec2
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-03-06 11:27:23 +0300

    feat(router): Removing comments in router and review router structer

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -14,8 +14,6 @@ const routes: RouteRecordRaw[] = [
     ],
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: "/:catchAll(.*)*",
     component: () => import("pages/ErrorNotFound.vue"),
